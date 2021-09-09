4f253a4a68906b7e4209e32cae9b91a67f97d147
Sara-Clarin <sclarin2@nd.edu>
2021-09-08 20:44:18 -0400

    Zero case

--- challenge03/program.py
+++ challenge03/program.py
@@ -93,6 +93,9 @@ def main():
             if numlist[0] < numlist[n-1]:
                 index1 = find_target_binary( numlist, 0, n -1, target)
                 print(f'{target} found at index {index1}') if index1 != -1 else print(f"{target} not found")
+            
+            elif len(numlist) == 1 and numlist[0] == target:    
+                print(f'{target} found at index 0')
 
             elif numlist[pivot] == target:
                 print(f'{target} found at index {pivot}')
