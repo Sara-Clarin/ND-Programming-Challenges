cfb122b0af324282f93a777743e98b905e26d541
Sara-Clarin <sclarin2@nd.edu>
2021-09-13 21:20:55 -0400

    all test cases OK except one

--- challenge05/program.cpp
+++ challenge05/program.cpp
@@ -2,15 +2,23 @@
 #include<string>
 #include<map>
 #include<algorithm>
+#include<cstring>
 
 using namespace std;
 
-void find_max_cycle( int i, int j){
-    map<int, int> collatz;
-    int n, count, max_length, max_key;
-    //printf("i is: %d, j is %d", i, j);
+#define ll long long
 
-    for ( int a = i; a <= j; a++){
+void find_max_cycle( ll i, ll j){
+    map<int, int> collatz;
+    int  max_length, max_key;
+    ll n, count, start, end;
+    //printf("i is: %d, j is %d", i, j);
+    if (i > j){
+        start = j; end = i;
+    } else {
+        start = i; end = j;
+    }
+    for ( int a = start; a <= end; a++){
         n = a;
         count = 1;
         while ( n > 1){
@@ -19,8 +27,11 @@ void find_max_cycle( int i, int j){
             if (n % 2)
                 n = 3*n + 1;
 
-            else
+            else{
                 n = n/2;
+            }
+         
+            //count += 1;
         }
         
          collatz[count] = a;
@@ -29,40 +40,42 @@ void find_max_cycle( int i, int j){
 
     /*for (auto e: collatz){
         cout << e.first << " , " << e.second << endl;
-    } */
+    } dk */
 
 
     // Find and report max count and argmax count
     auto m = std::max_element(
         std::begin(collatz), std::end(collatz)
+        
     );
 
     //cout << "maximum: ";
     cout << i<< " " << j<< " " <<  m->second << " " << m->first << endl;
-
+    collatz.clear();
 
 }
 int main(int argc, char * argv[]){
 
     int i; int j;
-    char* buf1;
+    char* buf1; char* buf2;
     char buffer[BUFSIZ];
     
     while (fgets(buffer, BUFSIZ, stdin)){
-         buf1  = strtok( buffer, " " );
-         i = atoi( buf1);
-         buf1 = strtok(NULL, " ");
-         j = atoi(buf1);
-
+     /* while( cin >> buf1 ){
+        //cin >> buf1;
+        cin >> buf2;
+        i = stoi(buf1);
+        j = stoi(buf2);
         //printf(" i is: %d, j is %d", i, j);
-       
+       */
+        buf1 = strtok( buffer, " ");
+        i = atoi( buf1);
+        buf1 = strtok(NULL, " ");
+        j = atoi(buf1);
+
         find_max_cycle( i, j);
 
-
     }
-
-
-
 
 return 0;
 }
