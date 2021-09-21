e2f938654330bf472b95deb6d6a047cfe36adbcd
Sara-Clarin <sclarin2@nd.edu>
2021-09-21 12:53:21 -0400

    challenge07: complete :

--- challenge07/program.cpp
+++ challenge07/program.cpp
@@ -5,47 +5,12 @@
 #include<string>
 #include<numeric>
 
+/*
+Challenge07: finding unique subsets of sum 0 with size 3
+
+*/
 using namespace std;
 
-void print_triplet( vector<int> &s){
-    vector<int> v = s;
-    //sort(v.begin(), v.end());
-    int n = v.size();
-    for (size_t i = 0; i < n -1; i++){
-        cout << v[i] << " + ";
-    }
-    cout << v[n -1] << endl;
-
-}
-
-void get_triplets( vector<int> &s, vector<int> &d, size_t k){
-        /* s: current elements
-            d: set of elements
-            k: current element to consider
-        */
-        // base case
-        if (k == d.size()){
-            int sum = accumulate(s.begin(), s.end(), 0);
-            if (sum == 0 && s.size() == 3){
-                /*for (auto a: s){
-                    cout << a << " + ";
-                        }
-                cout << endl;
-                */
-                print_triplet(s);
-            }
-    
-        }
-        else{
-            // don't take current element
-            get_triplets(s, d, k+1);
-            // take current element
-            s.push_back( d[k]);
-            get_triplets( s, d, k+1);
-            s.pop_back( );
-        }
-
-}
 
 void find_triplets_iterative( vector<int> &s){
 
@@ -57,7 +22,7 @@ void find_triplets_iterative( vector<int> &s){
             continue;
         }
 
-        // start from second index and iterate all other options
+        //  iterate all other options after current index
         while (mid < end){
             if ( end < s.size() -1 && s[end] == s[end +1]){  // duplicates adjacents
                 end --;
@@ -75,26 +40,19 @@ void find_triplets_iterative( vector<int> &s){
                 end--;
             }
          }
-
-
     }
-    // cout << endl;
-
 }
 
 int main( int argc, char **argv){
-    char * buf;
-    char buffer[BUFSIZ];
     string line;
     int i;
     int  co = 0; 
-    vector<int> s; int c;
-    //ifstream ifs; 
+    vector<int> s;
 
-    //while ( getline(cin, line)){
     while( getline(cin, line) ){
-       // getline(cin, line);
-       if (co != 0){ cout << endl; }
+       if (co != 0){              // ensure correct formatting
+         cout << endl; 
+         }
 
         istringstream ss(line);
         vector<int> d;
@@ -103,33 +61,10 @@ int main( int argc, char **argv){
         }
 
         sort(d.begin(), d.end());
-        /*
-        for (auto e: d){
-            cout << e << " ";
-        } cout << endl; */
 
         find_triplets_iterative(d );
-        s.clear();
+        s.clear();                  // space efficiency
         d.clear();
-        //cout << endl;
-        /*
-         scanf( "%d", &c);
-        if (c != EOF)  {
-            cout << endl;
-        }   */
-        /*
-        auto itr = line.end() ;
-        cout << endl;
-        cout << *itr;
-        if (*itr == EOF){
-            cout << "end found!";
-        }
-        cout << endl;*/
-        /*
-        size_t found = line.find("eof");
-        if ( found != string::npos){
-            cout << "FOUND! ";
-        } */
         co++;
     }
 
