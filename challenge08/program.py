e65f6a21b11948760ad95404e77b51f445bb4816
Sara-Clarin <sclarin2@nd.edu>
2021-09-22 18:58:01 -0400

    account for the 5 (do nothing) case

--- challenge08/program.py
+++ challenge08/program.py
@@ -6,47 +6,74 @@ import sys
 MOVES = {
     '1': '68',
     '2': '79',
-    '3': '49',
-    '4': '039',   # omit 5 because it's never solution
+    '3': '48',
+    '4': '039',  
+    '5': ' ',
     '6': '017',
     '7': '26',
     '8': '13',
     '9': '24',
-    '0': '46'
+    '0': '46',
+    ' ' : ''
 }
 
-def find_rook_moves( N, start , sequence):
+#results = []
+
+def find_rook_moves( N, start , sequence, totalmoves):
     '''
     N: the number of steps to continue recursing
     start: the current number
     sequence: the sequence we're building
     '''
     
-
     # base case: when N = 0 or we've done N steps
-    if ( N == 0):
+    #if ( N <= 0):
+    if len(sequence) == totalmoves:
         return [sequence]
         #print(sequence)
+        #return
         
     # recursive step
     results = []
-    for move in MOVES[start]:
+    #results.extend( sequence)
+    count = 1
+    for ind, move in enumerate(MOVES[start]):
         #print(move)
         #sequce = sequence +  (find_rook_
-        results.extend(
+         #if count == 2:
 
-            find_rook_moves( N-1,  move  , sequence + start )
-        )
+         
+         #dummy = find_rook_moves( N-1,  move  , sequence + start, totalmoves )
+         #if ind > 1 and results[ind - 1 ] != dummy:
+         results.extend(
+             find_rook_moves( N-1,  move  , sequence + start, totalmoves )
+         )
+         #count += 1    
+    
+    results = remove_duplicates(results)
+    return results
 
+def remove_duplicates( results):
+
+    for ind, value in enumerate(results):
+        if ind >= 1 and value == results[ind -1]:
+            del results[ ind -1]
+            
     return results
 
 
 def main():
+    count = 0
     for line in sys.stdin:
         start, hops = line.strip().split()
-        print(f'{start} {hops}')
-        sequence = find_rook_moves( int( hops) , start, "")
-        print(sequence)
+
+        if count > 0:
+            print("")
+
+        for path in find_rook_moves( int( hops)  , start, "", int(hops)):
+            print(path)
+
+        count += 1
 
 if __name__ == "__main__":
     main()
