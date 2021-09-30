d6b36495f853c6be83dd208ec37aaa2122c9d914
Sara-Clarin <sclarin2@nd.edu>
2021-09-29 21:04:00 -0400

    challenge10: done

--- challenge10/program.py
+++ challenge10/program.py
@@ -5,18 +5,32 @@ import sys
 
 LEGOS = [ "4", "3", "2", "1" ]
 
-
 def brick_rows_greedy( legolist ):
     ''' 
     legolist: sorted list of bricks
     rows: the minimum number of rows required to store
+    strategy: place the largest legos first
     '''
     rows = 0
 
-    
+    while legolist:
 
+        lego = legolist.pop(0)
+        remainder = 4 - lego
+
+        i = 0
+        while remainder > 0 and i < len(legolist):
+ 
+            if legolist[i] <= remainder:
+                lego2 = legolist.pop(i)
+                remainder -= lego2
+            else:
+               i  += 1
+
+        rows += 1
 
     return rows
+
 
 def build_legolist( numlist ):
     '''
@@ -29,26 +43,21 @@ def build_legolist( numlist ):
     numlist.reverse()
     index = 0
     for legocount in numlist:
-        #print(index)
         for i in range(legocount):
-           # print(i)
-            legolist.append( LEGOS[index] )
+            legolist.append( int(LEGOS[index] ) )
 
         index += 1
 
-    #print(legolist)
+    return legolist
 
 
 def main():
 
-
     for line in sys.stdin.readlines():
         line = list( map(int, line.strip().split()))
-        
-        print(line)
-        legolist = build_legolist( line) 
-        rows = brick_rows_greedy( legolist )
-
+        legolist = build_legolist( line)
+        rows =  brick_rows_greedy( legolist)
+        print(rows)
 
 if __name__ == "__main__":
     main()
