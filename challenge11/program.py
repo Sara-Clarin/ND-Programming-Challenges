da834c8aacc6e484fd6521a0a9727879c605bda3
Sara-Clarin <sclarin2@nd.edu>
2021-10-04 18:31:48 -0400

    challenge11: I actually did it omg what

--- challenge11/program.py
+++ challenge11/program.py
@@ -1,57 +1,44 @@
 #!/usr/bin/env python3
 
+# Author: Sara Clarin
+# Sussy Dogecoin Transactions: program to calculate the minimum number of steps required to reach input target, when previous value can only be doubled or +1
+#
+# 4 October 2022
 
 import sys
 
-# create cache
 
+# Functions
 
-# functions
-
-def get_dogecoins( n, tx, table={} ):
+def get_dogecoins( n,  table={} ):
 
     # initialize table, insert base cases
     table = [0] * (n+1)
 
     table[0] = 0
     table[1] = 1
-
-    #base case: except no recursion??
-    #if n == 1:
-        #return table[1]
-
-    tx = 0
-  
+ 
     # Build bottom-up table of optimal solutions
     for i in range(2, n + 1):
+
         if not table[i]:
 
-            if i % 2 :     # n is odd
+            if i % 2 :                   # n is odd
                 table[i] = table[i-1] + 1
-            else:           # n is even
+            else:                        # n is even
                 table[i] = min(table[i//2], table[i-1]) + 1
-    #print(coinsum)
-
-    print("Table:")
-    count = 0
-    for item in table:
-        print(f't[{count}]: {item}')
-        count += 1
 
     return table[n]
 
-
-# main execution
-
+# Main Execution
 def main():
 
     cache = {}
-
     for index,target in enumerate(map(int, sys.stdin), 1):
-        print(f'input: {target}')
-
         min_tx = get_dogecoins( target, 0 , cache)
         print(f'{index}. Minimum number of doge transactions: {min_tx}')
+
+
 
 if __name__ == "__main__":
     main()
