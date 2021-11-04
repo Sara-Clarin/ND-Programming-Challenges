613df19c1f4eba7a46e768d0c732a02f1cf04820
Sara-Clarin <sclarin2@nd.edu>
2021-11-04 19:23:19 -0400

    recursive version worked

--- challenge15/program.py
+++ challenge15/program.py
@@ -11,51 +11,105 @@ def read_graph(  m  ):
     graph: { node: {}}
     '''
     g = collections.defaultdict( list) # 
-    print(f' in function')
 
     for _ in range( m):
         source, target = sys.stdin.readline().split()
         g[source].append(target)
         #print(source, target)
         g[target].append(source)
-    print(' end of input')
+    
     return g
 
-def DFS_len( v , g):
+def vertices_to_edge( u, v):
+    '''
+    input: two vertices of graph (chars)
+    output: a tuple with (v1, v2) in sorted order
+    '''
+    node_list = [u, v]
+    n1 = sorted(node_list)
+    #print('should print now')
+    return tuple( i for i in n1 )
+
+
+def DFS_len( v ,  g):
     '''
     Depth-first search of an undirected graph, visiting an edge only once
+    idea: 
     '''
     frontier = [ ]
-    visited = { }
+    visited = set()
     frontier.append(v)
-    pathcnt = 0
+    #frontier.append( edge)
+    pathcnt = -1            # start with -1 so that first iteration counts first edge
+    paths = []
 
     while len(frontier) > 0:
         v = frontier.pop()
         print(f' v is: {v}')
 
-        if v in visited:    # check to see if an edge was visited or not
-            continue
+        #if edge in visited:    # check to see if an edge was visited or not
+            #continue           # probably will delet
 
-        visited[v] = True  # add edge to visited list
-        print(v)
+        #visited[edge] = True  # add edge to visited list
+        
         pathcnt += 1
+        children = 0
 
         for u in g[v]:
-            frontier.append( u)
+            edge = vertices_to_edge( u, v)
 
-    return pathcnt
+            if edge not in visited:  # only continue down edges we haven't seen yet
+                #print(edge)
+                visited.add( edge )
+                frontier.append( u)
+                children += 1
+            #visited.remove(edge)   
+            # we need to backtrack in here somewhere
+
+        # idea: when we recurse back up, we subtract how far in we are
+        if children == 0:
+            paths.append(pathcnt)
+            pathcnt = 0
+
+    return max(paths)
+
+def DFS_rec( cnt, g, node, visited, paths):
+
+    for u in g[node]:
+        edge = vertices_to_edge( u,  node)
+        #print( edge )
+        # base case: 
+        if edge in visited:
+            continue
+
+        # recursive case
+        else:
+            visited.add( edge)
+            DFS_rec( cnt + 1, g, u, visited, paths)
+            visited.remove(edge)
+
+    paths.append(cnt)
+    # recursive step: visit adjacent nodes
+    
+    return max( paths)
 
 def Compute_Longest( g ):
     '''
     call DFS-length on each node in the graph and return highest
     '''
-    paths = 0
-    print('in Compute_longest')
-    first = list(g.keys())[0][0] 
-    paths = DFS_len( first, g )
+    
+    first_v = list(g.keys())[0][0]
+    second = list(g.values())[0][0]
+    #first_e = (first_v, second)
+    #print(first_e)
 
-    return paths
+    # for key, val in g.items():
+    #paths = DFS_len( first_v,    g )
+    max_path = 0
+    for key in g.keys():
+        max_path = max( max_path, DFS_rec( 0, g, key, set() , []) )
+    
+    return max_path
 
 
 def main():
@@ -67,17 +121,19 @@ def main():
     while True:
         sizes = sys.stdin.readline()
         n, m = sizes.split()
-        print(f'{m} {n}')
+        #print(f'{m} {n}')
 
         if m == '0' and n == '0':
-            print('breaking')
             break
 
         g = read_graph( int(m) )
-        print(g)
+        #print(g)
 
-        longest = Compute_Longest(g)
-        print(longest)
+        #print(f'Sorting edges :))')
+        #print( vertices_to_edge( m, n))
+
+        print( Compute_Longest(g) )
+        #print(f'longest path: {longest}')
 
 if __name__ == "__main__":
     main()
