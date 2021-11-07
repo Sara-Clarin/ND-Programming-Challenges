1147349b274dd81af337607809c3e779c494cfb3
Sara-Clarin <sclarin2@nd.edu>
2021-11-07 13:59:47 -0500

    challenge16: done

--- challenge16/program.py
+++ challenge16/program.py
@@ -4,9 +4,6 @@ from dataclasses import dataclass
 import collections
 import heapq
 
-@dataclass
-class Cell:
-    id : int
 
 def getID( row, col, COLS):
     return row*COLS + col
@@ -14,10 +11,8 @@ def getID( row, col, COLS):
 def Get_Matrix( row, col):
     ''' create a matrix representation of input
     iterate through the matrix to make undirected graph
-    maybe our matrix will store the ID at a cell if it's not 1
-    # in iteration, we'll calculate cell's costs
-    # graph:  ID : { neighborID: cost, NeighborID2: cost2 }
-    # output: graph, start, end[]
+    # graph format -->  ID : { neighborID: cost, NeighborID2: cost2 }
+    # output --> graph, start, end[]
     '''
     CLOSED = 1000
     start = 0
@@ -25,42 +20,38 @@ def Get_Matrix( row, col):
 
     matrix = [ [0 for i in range(col ) ] for i in range(row) ]
 
-    for i in range(row ):
+    for i in range(row ):               # create matrix with value = ID if cell is open   
         rowlist = sys.stdin.readline()
         rowlist = rowlist.split()
-        #print(rowlist)
 
-        for j in range(col ):
+        for j in range(col ):           
             if rowlist[j] == 'S':          # set start and end variables
                 start = getID(i,j,col)
                 matrix[i][j] = start
             elif rowlist[j] == 'E':
-                #print("adding the end val")
-                end.append( getID(i,j,col))
-                matrix[i][j] = getID(i, j, col)  # change this so 1 function call
+                e = getID(i, j, col)
+                end.append( e )
+                matrix[i][j] = e  
             elif rowlist[j] != '1':
                 matrix[i][j] = getID(i, j, col)
             else:
                 matrix[i][j] = CLOSED
         
-    # pad the left with sentinels
+    # pad the left and top with sentinel value -50
     for i in range(row):
         matrix[i].insert( 0,-50 )
 
     matrix.insert(0, [ -50 for i in range(col + 1) ])
-    #print( matrix)
-    #print(f'END in function: {end}')
     return start, end, matrix
     
 def Matrix_to_adjList( matrix, row, col):
-
+    '''Create an undirected graph adjacency list given matrix of ID's
+    '''
     g = collections.defaultdict(dict)
     CLOSED = 1000
-    #print(matrix)
 
     for i in range(1, row  + 1):    
         for j in range(1,col + 1 ):
-            #print(f'cell : {matrix[i][j]}')
             cell = matrix[i][j]
             if cell == CLOSED:
                 continue
@@ -68,25 +59,21 @@ def Matrix_to_adjList( matrix, row, col):
             # 4 NONDIAGONAL DIRECTIONS: weight = 1
             # top
             if matrix[ i - 1][j] != -50 and matrix[i - 1][j] != CLOSED: 
-                #print(f"top exists: {matrix[i-1][j]}")
                 top = matrix[i - 1][j]
                 g[ cell ][ top] = 1         # g[src][trget] = wt
                 g[top][cell] = 1
             # bottom
             if i != row and matrix[i + 1][j] != CLOSED:
-                #print(f"bottom exists: {matrix[i + 1][j]}")
                 bottom = matrix[i+1][j]
                 g[cell][bottom] = 1
                 g[bottom][cell] = 1
             # right
             if j != col and matrix[i][j+1] != CLOSED:
-                #print(f"right exists: {matrix[i][j + 1]}")
                 right = matrix[i][j + 1]
                 g[cell][right] = 1
                 g[right][cell] = 1
             # left
             if j != 1 and matrix[i][j - 1] != CLOSED:
-                #print(f"left exists: {matrix[i][j - 1]}")
                 left = matrix[i][j -1]
                 g[cell][left] = 1
                 g[left][cell] = 1
@@ -112,12 +99,13 @@ def Matrix_to_adjList( matrix, row, col):
                 botright = matrix[i+1][j+1]
                 g[cell][botright] = 2
                 g[botright][cell] = 2
-
-    #print(g)
     return g
 
 def compute_sssp( g, start, end):
-
+    '''Compute the single source shortest paths from start 
+    visited --> optimal- traversal dictionary
+    end --> list of end values
+    '''
     frontier = []   # nodes reachable but not visited
     visited = {}    # nodes we've visited : weights
     distance = 0
@@ -131,21 +119,15 @@ def compute_sssp( g, start, end):
             continue
 
         visited[target] = source
-        #visited[target] = (source, distance)
-        #visited[target][source] = distance
-
-        #if target == end:  
-            #print(f'end: {end}') 
-            #break
-
+    
         for neighbor, weight in g[target].items(): 
             heapq.heappush( frontier, (distance + weight, target, neighbor ))
 
     return visited
 
 def Reconstruct_Path( graph, visited, source, target ):
-
-    #print(f'visited: {visited}')
+    ''' Retraces the visited dictionary to pull out the path from source --> target
+    '''
     path = []
     curr = target
     cost = 0
@@ -157,13 +139,10 @@ def Reconstruct_Path( graph, visited, source, target ):
             cost += graph[curr][next]
             curr = next
         except KeyError:
-            #print(f"Key error!!! {curr}")
             return 0, []
     
-    #print(f'Cost: {cost}')
     path.append(source)
     path.reverse()
-    #print(f'Path: {path}')
     return cost, path
 
 def main():
@@ -179,8 +158,6 @@ def main():
             break
 
         start, end, matrix = Get_Matrix( r, c )
-        #print(f'START: {start} END: {end}')
-        #print(f'END: {end}')
         g = Matrix_to_adjList( matrix, r, c )
 
         mincost = sys.maxsize
