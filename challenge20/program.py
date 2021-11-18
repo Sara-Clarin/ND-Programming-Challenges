d99328ce03983a7769daf3e0418f8ec4ace7cb0c
Sara-Clarin <sclarin2@nd.edu>
2021-11-18 13:32:04 -0500

    challenge20: complete

--- challenge20/program.py
+++ challenge20/program.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 
+
+# Maximum Bandwidth: a program that determines the maximum flow of an undirected graph
+# 18 November 2021
+
+
 import sys, collections
 
 
 def read_graph( connections):
+    '''
+    Create a weighted graph with n connections from stdin
+    '''
     graph = collections.defaultdict(dict)
 
     for _ in range(connections):
@@ -16,36 +24,28 @@ def read_graph( connections):
 
 def find_bfspath( graph, start, weight, sink):
     '''
-
+    Iterative BFS traversal from start --> sink
     '''
     visited = { }
-    #frontier = [ src]  # use "queue" for BFS traversal
     frontier = collections.deque(  )
     frontier.append( (start, start, weight))
-    src = 1
-    trg = 1
-    path = []
 
-    #print(f'frontier: {frontier[0]}')
     while frontier:
         src, trg, weight = frontier.popleft()
-        #w = frontier.popleft()
-        #print(w)
+
         if trg in visited:
             continue
 
         if trg == sink: 
             visited[sink] = src
-            #visited[src] = sink
-            #print( sink)
             break
 
-        #print(src, trg, weight) 
         visited[trg] = src
 
         for neighbor, capacity in graph[trg].items():
             if capacity > 0:
                 frontier.append( ( trg, neighbor , capacity ))
+
 
     if sink in visited:             # if we can't get to the sink, we've traversed all src-->sink paths
         return visited
@@ -53,6 +53,10 @@ def find_bfspath( graph, start, weight, sink):
         return None
 
 def compute_min_flow( graph, visited, start, sink ):
+    '''
+    Traverse visited from sink --> start and record the most recent path
+    Find the bottleneck point along the path ( min_cost)
+    '''
     path = []
     min_cost = sys.maxsize
     curr = sink
@@ -66,59 +70,38 @@ def compute_min_flow( graph, visited, start, sink ):
         path.append( curr)
 
     path.reverse()
-    #print(f'{path} has min flow {min_cost}')
     return path, min_cost
 
 def reduce_capacity( graph, path, min_flow):
     '''
     Go through every pair of adjacent nodes in path
-    decrement the edge weight by min_flow
+    Decrement the edge weight by bottleneck weight
     '''
 
     for i in range( 1, len( path)):
         src, trg = path[i], path[ i-1]
-        #print(src, trg)
         graph[src][trg] -= min_flow
         graph[trg][src] -= min_flow
 
-    #print(f' Graph decremented: {graph}')
     return graph
 
 def main():
-    '''
-    visited = { 1 }
-    for n in sys.stdin:
 
-        n = int(n.strip())
-        if n == 0:
-            break
-        src, trg, connections = map(int, sys.stdin.readline().strip().split())
-        print(n, src, trg, connections)
-      
-        graph = read_graph( connections )
-        print(graph)
-        visited = find_dfspath( graph, src, 0, trg )
-     '''
+    # INITIAL CONDITIONS
     n = int(sys.stdin.readline().strip() )
     src, sink, connections = map(int, sys.stdin.readline().strip().split())
     visited = { }
     iterations = 0
     
-    #while visited or iterations == 0 or iterations < 10:
     while True:
         graph = read_graph(connections)
         visited = find_bfspath( graph, src, 0, sink )
         bandwidth = 0
-        #print(f'visited: {visited}')
 
         while visited:  
-            #print(graph)
-
             path, minimum_flow = compute_min_flow( graph, visited, src, sink)
             graph = reduce_capacity( graph, path, minimum_flow) 
             visited = find_bfspath( graph, src, 0, sink )
-
-            #print(f'visited: {visited}')  
             bandwidth += minimum_flow
 
         iterations += 1
@@ -128,8 +111,6 @@ def main():
         if n == 0:
             break
         src, sink, connections = map(int, sys.stdin.readline().strip().split())
-
-        
 
 if __name__ == "__main__":
     main()
