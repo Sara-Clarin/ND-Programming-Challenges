5422d151e5f4d1130014843cda3e7f660cfc8e9e
Sara-Clarin <sclarin2@nd.edu>
2022-05-27 14:06:06 -0600

    c6-c11

---
+++ challenge06/attempt1.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+# Sara Clarin
+# Challenge06: find longest sequence of consecutive, distinct items in a list
+
+
+import sys
+fruitDict = {}
+sequenceDict = {}
+
+def retrieve_sequence( fruitDict):
+    #seq_list = []
+    #for fruit, val in sequenceDict.items():
+     #   seq_list.append(fruit)
+
+    possible_seq = tuple(fruitDict.keys())
+    sequenceDict[possible_seq] = len(possible_seq)
+    fruitDict.clear()
+
+    #print( possible_seq)
+    #return possible_seq
+
+
+def find_distinct_fruits( line):
+    sequence = []
+    max_sequence = 0
+    left = 0
+    right = 1
+
+    for fruit in line:
+        try:                    # we have a collision
+            temp = fruitDict[fruit] 
+            retrieve_sequence(fruitDict)
+        
+            #fruitDict.clear()
+
+        except KeyError:        # not yet seen
+             fruitDict[fruit] = 0
+
+    if len(fruitDict) > 0:
+        retrieve_sequence( fruitDict)
+        
+    
+    max_sequence = max(sequenceDict, key=sequenceDict.get)
+    fruitDict.clear()
+    sequenceDict.clear()
+    return max_sequence
+
+
+def main():
+
+    for  line in sys.stdin.readlines():
+       max_sequence = find_distinct_fruits( line.strip().split())
+       l = len(max_sequence)
+       print(f'{l}: {max_sequence}')
+
+
+if __name__ == "__main__":
+    main()
